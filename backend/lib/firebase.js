4fa2e261d19b4363995a34e685449a270e2f73e9
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-11-01 05:15:31 +0530

    added multer

--- backend/lib/firebase.js
+++ backend/lib/firebase.js
@@ -1,43 +1,36 @@
-const {initializeApp} = require('firebase/app')
-const {getFileStore} = require('firebase/firestore')
-const dotenv = require('dotenv')
-dotenv.config()
-
-
-const firebase = require('firebase/app')
-const {getStorage,ref,uploadBytes,getDownloadURL} = require('firebase/storage')
+const { initializeApp } = require("firebase/app");
+// const {getFileStore} = require('firebase/firestore')
+const dotenv = require("dotenv");
+const multer = require("multer");
+const { getStorage } = require("firebase/storage");
+dotenv.config();
 
 const {
-    API_KEY,
-    AUTH_DOMAIN,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
-    MEASUREMENT_ID
-
-
-} = process.env
-
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} = process.env;
 
 const firebaseConfig = {
-    apiKey:API_KEY,
-    authDomain:AUTH_DOMAIN,
-    projectId:PROJECT_ID,
-    storageBucket:STORAGE_BUCKET,
-    messagingSenderId:MESSAGING_SENDER_ID,
-    appId:APP_ID,
-    measurementId:MEASUREMENT_ID
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
 
-}
-const firebaseApp  = firebase.initializeApp(firebaseConfig)
-const storage = getStorage(firebaseApp)
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
 
-
-
+const upload = multer({ storage: multer.memoryStorage() });
 
 module.exports = {
-    
-    storage,
-
-}
+  storage,
+  upload,
+};
