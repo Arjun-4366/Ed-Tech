8c453049f68cb5f1b42f08ba924d9f66c879cb01
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-11-07 02:21:17 +0530

    changed date format

--- backend/controller/firebaseController.js
+++ backend/controller/firebaseController.js
@@ -19,17 +19,16 @@ const uploadVideoFiles = async (req, res) => {
   }
 };
 
-const fetchFirebaseVideos = async(res,req)=>{
+const fetchFirebaseVideos = async(req,res)=>{
 try{
 
-  const file  = bucket.file("Screen Recording 2024-08-02 171317.mp4")
+  const file  = bucket.file("ReactJS.mp4")
   const [url] = await file.getSignedUrl({
     action:"read",
-    expires:"30-01-2030"
+    expires: new Date(Date.now()+7*24*60*60*1000)
   })
 
-// const videoRef = ref(storage,"gs://bright-minds-a8916.appspot.com/Screen Recording 2024-08-02 171317.mp4")
-// const videoDownloadUrl = await(getDownloadURL(videoRef))
+console.log("generated video url",url)
 res.json({Videourl:url})
 
 }
