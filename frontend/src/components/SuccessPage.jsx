031fddb6140327085d13a1998148de35eb29df7d
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-11-07 02:22:42 +0530

    added button

--- frontend/src/components/SuccessPage.jsx
+++ frontend/src/components/SuccessPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 function SuccessPage() {
+  const navigate = useNavigate()
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center flex-col justify-center h-screen">
       <h1 className="text-green-500 text-3xl">Payment Successful!</h1>
+      <button onClick={()=>navigate("/playVideo")} className='bg-green-500 p-2 rounded-lg mt-4 text-white'>Go to course</button>
     </div>
   )
 }
