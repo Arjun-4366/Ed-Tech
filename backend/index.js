e1fc519ae3f400dfb9167d845ad38493cbb3ad6f
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-11-08 01:56:37 +0530

    intsalled cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-const cors = require('cors');
+const cors = require("cors")
 app.use(cors({
     origin:'https://ed-tech-kc2p.vercel.app',
     methods:'GET,POST,PUT,DELETE',
